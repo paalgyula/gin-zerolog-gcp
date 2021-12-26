0c813cebb1e093f305131a0bafe2901408a26685
Gyula Paal <paalgyula@paalgyula.com>
2021-12-26 16:54:44 +0100

    Extended logger with config options

--- logger.go
+++ logger.go
@@ -2,19 +2,58 @@ package gcp
 
 import (
 	"os"
+	"time"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
-// SetupLogger sets up the logger for GKE log friendly format.
-func SetupLogger(debug bool) {
-	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+type loggerConfig struct {
+	pretty   bool
+	logLevel zerolog.Level
+}
 
-	if debug {
-		// nolint:exhaustivestruct
-		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
-		log.Logger = log.With().Caller().Logger()
+// LoggerOption function definition for configuring GCP logger
+type LoggerOption = func(*loggerConfig)
+
+// You can turn on pretty human readable format instead of json output
+func WithPrettyOutput() LoggerOption {
+	return func(opt *loggerConfig) {
+		opt.pretty = true
+	}
+}
+
+// WithLogLevel you can specify the logging level
+func WithLogLevel(lvl zerolog.Level) LoggerOption {
+	return func(opt *loggerConfig) {
+		opt.logLevel = lvl
+	}
+}
+
+// SetupLogger sets up the logger for GKE log friendly format.
+func SetupLogger(opts ...LoggerOption) {
+	opt := &loggerConfig{
+		pretty:   false,
+		logLevel: zerolog.InfoLevel,
+	}
+
+	for _, o := range opts {
+		o(opt)
+	}
+
+	zerolog.SetGlobalLevel(opt.logLevel)
+
+	if opt.pretty {
+
+		log.Logger = log.Output(zerolog.ConsoleWriter{
+			Out:        os.Stdout,
+			NoColor:    false,
+			TimeFormat: time.Kitchen,
+		})
+
+		log.Logger = log.With().
+			Caller().
+			Logger()
 
 		return
 	}
